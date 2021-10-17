e8e9f0985147457515a941a6d59f545556e0acf9
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-17 13:29:32 -0300

    Adds request to dog.ceo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from './components/Image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.fetchData = this.fetchData.bind(this);
+    this.state = {
+      source: '',
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const responseJSON = await response.json();
+    const imgSrc = responseJSON.message;
+    this.setState({ source: imgSrc });
+  }
+
+  render() {
+    const { source } = this.state;
+    return (
+      <div>
+        <Image src={ source } />
+      </div>
+    )
+  }
 }
 
 export default App;
